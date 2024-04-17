86d0446c7823615b6367bdf919db331f3188a17b
AndyTung401 <tungchengwei@icloud.com>
2024-04-17 14:25:37 +0800

    Initial Commit

---
+++ Speedometer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speedometer
+//
+//  Created by 董承威 on 2024/4/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by 董承威 on 2024/4/17.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
